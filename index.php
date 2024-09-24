85b7fab1cc008e28bf9fa8e7a3002785a96a7a3d
RachataKH <Toon2574725@gmail.com>
2024-09-24 12:08:25 +0700

    not_verify

--- index.php
+++ index.php
@@ -7,41 +7,43 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-    <h1 style="text-align: center;">WEBTOON</h1>
-    <hr>
-    <div>
-        หมวดหมู่ : <select name="--ทั้งหมด--" id="">
-            <option value="--ทั้งหมด--">--ทั้งหมด--</option>
-            <option value="เรื่องทั่วไป">เรื่องทั่วไป</option>
-            <option value="เรื่องเรียน">เรื่องเรียน</option>
-        </select>
-
+    <div class="container-lg">
+        <h1 style="text-align: center;" class="mt-3">WEBTOON</h1>
+        <?php include "nav.php" ?>
+    </div>
+    <div class="mt-4">
+        <label>หมวดหมู่</label>
+        <span class="dropdown">
+            <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">--ทั้งหมด--</button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item btn btn-outline-secondary" href="#">--ทั้งหมด--</a></li>
+                    <li><a class="dropdown-item" href="#">เรื่องทั่วไป</a></li>
+                    <li><a class="dropdown-item" href="#">เรื่องเรียน</a></li>
+                </ul>
+        </span>
         <?php
-        if(!isset($_SESSION['id'])){
-        echo "<a href='login.php' style='float: right;'>เข้าสู่ระบบ</a>";
-        }else{
-        echo "<span style='float : right'>
-        ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;
-        <a href='logout.php' style='float: right;'>ออกจากระบบ</a><br>
-        </span>";
-            echo "<div><a href='newpost.php';'>สร้างกระทู้ใหม่</a></div>";
+        if(isset($_SESSION['id'])){
+            echo"<a href='newpost.php' class='btn btn-success btn-sm' style='float: right;'><i class='bi bi-plus'></i>สร้างกระทู้ใหม่</a>";
         }
         ?>
-        <ul>
-            <?php
+    </div>
+    <table class="table table-striped table-secondary mt-4">
+        <?php
             for($i=1;$i<=10;$i=$i+1)
             {
-                echo "<li><a href=post.php?id=$i>กระทู้ที่ $i</a>";
+                echo "<tr><td><a href=post.php?id=$i style='text-decoration : none'>กระทู้ที่ $i</a>";
                 if (isset($_SESSION['id']) && $_SESSION['role']=='a'){
-                    echo "&nbsp<a href='delete.php?id=$i'>ลบ</a>";
-
-                }
-            echo "</li>";
+                    echo "&nbsp;&nbsp;<a href='delete.php?id=$i' class='btn btn-danger btn-sm me-4' style='float: right;'><i class='bi bi-trash-fill'></i></a>";
+                    }
+                    echo "</td></tr>";
             }
-            ?>
-        </ul>
-    </div>
+        ?>
+    </table>
+    </span>
 </body>
 </html>
--- login.php
+++ login.php
@@ -9,22 +9,42 @@ if (isset($_SESSION["id"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Login</title>
 </head>
 <body>
-    <h1 style="text-align: center;">WEBTOON</h1>
-    <hr>
-    <form action="verify.php" method="post">
-        <table style="border: 2px solid black; width: 40%;" align="center">
-            <tr><td colspan="2" align="center" style="background-color: #6cd2fe;">เข้าสู่ระบบ</td></tr>
-            <tr><td>Login</td><td><input type="text" name="Login"></td></tr>
-            <tr><td>Password</td><td><input type="password" name="Password"></td></tr>
-            <tr><td colspan="2" align="center"><input type="submit" value= "Login"></td></tr>
-        </table>
-    </form>
-    <br>
-    <div style="text-align: center;">
-        ถ้ายังไม่ได้เป็นสมาชิก <a href="register.html">กรุณาสมัครสมาชิก</a>
+    <div class="container-lg">
+        <h1 style="text-align: center ;mt-4">WEBTOON</h1>
+        <?php include "nav.php" ?>
+        <div class="row">
+            <div class="col-sm-8 col-md-6 col-lg-4 mx-auto">
+                <div class="card mt-4">
+                    <h5 class="card-header">เข้าสู่ระบบ</h5>
+                    <div class="card-boby">
+                        <form action="verify.php" method="post">
+                            <div class="form-group mt-3">
+                                <label for="login" class="form-label">Password :</label>
+                                <input id="login" type="text" class="form-control" name="login">
+                            </div>
+                            <div>
+                                <label for="pwd" class="form-label">Password :</label>
+                                <input id="pwd" type="password" class="form-control" name="password">
+                            </div>
+                            <div class="mt-3 d-flex justify-content-center">
+                                <button class="btn btn-success btn-sm me-3" type="submit">Login</button>
+                                <button class="btn btn-danger btn-sm" type="reset">Reset</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div style="text-align: center;">
+            ถ้ายังไม่ได้เป็นสมาชิก <a href="register.html">กรุณาสมัครสมาชิก</a>
+        </div>
     </div>
 </body>
 </html>
